d7a783c46d1a33e4c43cf860d063240e1c08dda3
delano <delano@solutious.com>
2009-10-01 11:29:26 -0400

    Cleaned up Stella::Engine::Load; print prep, test, and reporting times

--- lib/stella/engine/load.rb
+++ lib/stella/engine/load.rb
@@ -19,50 +19,101 @@ module Stella::Engine
       Stella.li3 "Hosts: " << opts[:hosts].join(', ')
       Stella.li2 plan.pretty
       
-      packages = build_thread_package plan, opts
-      Stella.li $/, "Prepared #{packages.size} virtual clients..."
+      counts = calculate_usecase_clients plan, opts
+      
+      Stella.li $/, "Preparing #{counts[:total]} virtual clients...", $/
+      Stella.lflush
+      packages = build_thread_package plan, opts, counts
+      
+      Stella.li "Generating load...", $/
       Stella.lflush
       
+      execute_test_plan packages, opts[:repetitions]
       
-      Stella.li $/, "Starting test...", $/
+      Stella.li "Processing statistics...", $/
       Stella.lflush
-      sleep 0.3
+      generate_report plan
       
-      Thread.ify packages, :threads => opts[:clients] do |package|
-
-        Benelux.current_track package.client.gibbler
-        Benelux.add_thread_tags :usecase => package.usecase.gibbler_cache
-        
-        (1..opts[:repetitions]).to_a.each do |rep|
-          
-          Benelux.add_thread_tags :rep =>  rep
-          
-          # We store client specific data in the usecase
-          # so we clone it here so each thread is unique.
-          Stella::Engine::Load.rescue(package.client.gibbler_cache) { 
-            stats = package.client.execute package.usecase
-          }
-          
-          Benelux.remove_thread_tags :rep
-          
-          Benelux.update_track_timeline
-          
-        end
-        
-        Benelux.remove_thread_tags :usecase
+      rep_stats = self.timeline.ranges(:build_thread_package).first    
+      Stella.li "%20s %0.4f" % ['Prep time:', rep_stats.duration]
       
-      end
+      rep_stats = self.timeline.ranges(:execute_test_plan).first    
+      Stella.li "%20s %0.4f" % ['Test time:', rep_stats.duration]
       
-      generate_report(plan)
-      
-      # This will contain only the one call to generate_report
-      rep_stats = self.timeline.ranges.first
-      
-      Stella.li "Reporting time: %0.4f" % [rep_stats.duration]
+      rep_stats = self.timeline.ranges(:generate_report).first    
+      Stella.li "%20s %0.4f" % ['Reporting time:', rep_stats.duration]
       
       !plan.errors?
     end
     
+
+  protected
+    class ThreadPackage
+      attr_accessor :index
+      attr_accessor :client
+      attr_accessor :usecase
+      def initialize(i, c, u)
+        @index, @client, @usecase = i, c, u
+      end
+    end
+    
+    def execute_test_plan(packages, reps=1)
+      Thread.ify packages, :threads => packages.size do |package|
+        
+        # This thread will stay on this one track. 
+        Benelux.current_track package.client.gibbler
+        Benelux.add_thread_tags :usecase => package.usecase.gibbler_cache
+        
+        (1..reps).to_a.each do |rep|
+          Benelux.add_thread_tags :rep =>  rep
+          Stella::Engine::Load.rescue(package.client.gibbler_cache) {
+            stats = package.client.execute package.usecase
+          }
+          Benelux.remove_thread_tags :rep
+          Benelux.update_track_timeline
+        end
+        
+        Benelux.remove_thread_tags :usecase
+      end
+    end
+    def calculate_usecase_clients(plan, opts)
+      counts = { :total => 0 }
+      plan.usecases.each_with_index do |usecase,i|
+        count = case opts[:clients]
+        when 0..9
+          if (opts[:clients] % plan.usecases.size > 0) 
+            msg = "Client count does not evenly match usecase count"
+            raise Stella::Testplan::WackyRatio, msg
+          else
+            (opts[:clients] / plan.usecases.size)
+          end
+        else
+          (opts[:clients] * usecase.ratio).to_i
+        end
+        counts[usecase.gibbler_cache] = count
+        counts[:total] += count
+      end
+      counts
+    end
+    
+    def build_thread_package(plan, opts, counts)
+      packages, pointer = Array.new(counts[:total]), 0
+      plan.usecases.each do |usecase|
+        count = counts[usecase.gibbler_cache]
+        Stella.ld "THREAD PACKAGE: #{usecase.desc} (#{pointer} + #{count})"
+        # Fill the thread_package with the contents of the block
+        packages.fill(pointer, count) do |index|
+          Stella.li3 "Creating client ##{index+1} "
+          client = Stella::Client.new opts[:hosts].first, index+1
+          client.add_observer(self)
+          client.enable_nowait_mode if opts[:nowait]
+          ThreadPackage.new(index+1, client, usecase)
+        end
+        pointer += count
+      end
+      packages.compact # TODO: Why one nil element sometimes?
+    end
+      
     def generate_report(plan)
       Stella.li $/, " %-68s %s  ".att(:reverse) % [plan.desc, plan.gibbler_cache.shorter]
       plan.usecases.uniq.each_with_index do |uc,i| 
@@ -87,50 +138,8 @@ module Stella::Engine
         end
       end
     end
-  
-    Benelux.add_timer Stella::Engine::Load, :generate_report
-  
-  protected
-    class ThreadPackage
-      attr_accessor :index
-      attr_accessor :client
-      attr_accessor :usecase
-      def initialize(i, c, u)
-        @index, @client, @usecase = i, c, u
-      end
-    end
     
-    def build_thread_package(plan, opts)
-      packages, pointer = Array.new(opts[:clients]), 0
-      plan.usecases.each_with_index do |usecase,i|
-        
-        count = case opts[:clients]
-        when 0..9
-          if (opts[:clients] % plan.usecases.size > 0) 
-            msg = "Client count does not evenly match usecase count"
-            raise Stella::Testplan::WackyRatio, msg
-          else
-            (opts[:clients] / plan.usecases.size)
-          end
-        else
-          (opts[:clients] * usecase.ratio).to_i
-        end
-        
-        Stella.ld "THREAD PACKAGE: #{usecase.desc} (#{pointer} + #{count})"
-        # Fill the thread_package with the contents of the block
-        packages.fill(pointer, count) do |index|
-          Stella.li3 "Creating client ##{index+1} "
-          client = Stella::Client.new opts[:hosts].first, index+1
-          client.add_observer(self)
-          client.enable_nowait_mode if opts[:nowait]
-          ThreadPackage.new(index+1, client, usecase)
-        end
-        pointer += count
-      end
-      packages.compact # TODO: Why one nil element sometimes?
-    end
-      
-      
+    
     def update_prepare_request(client_id, usecase, req, counter)
       
     end
@@ -164,6 +173,10 @@ module Stella::Engine
       Stella.ld ex.backtrace
     end
     
+    
+    Benelux.add_timer Stella::Engine::Load, :generate_report
+    Benelux.add_timer Stella::Engine::Load, :build_thread_package
+    Benelux.add_timer Stella::Engine::Load, :execute_test_plan
   end
 end
 
