270b2dbcc3e03db3cfd237c5a61bd93cd16d4af0
delano <delano@solutious.com>
2009-10-02 06:31:28 -0400

    Added cookie example

---
+++ examples/cookies/plan.rb
@@ -0,0 +1,18 @@
+desc "Maintain Your Cookies"
+
+usecase 65, "Simple search" do
+  
+  get "/", "Homepage"
+  
+  get "/search", "Search Results" do
+    param :what  => random(['Big', 'Beads'])
+    param :where => 'Toronto'
+  end
+  
+  get "/", "Homepage" do
+    response 200 do
+      puts doc.css('ul#history').first
+    end
+  end
+  
+end
