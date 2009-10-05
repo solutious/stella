94dceba76c924f698903fd1a22d992595a5f2ed6
delano <delano@solutious.com>
2009-10-05 10:06:04 -0400

    Add TB

--- lib/stella/mixins/numeric.rb
+++ lib/stella/mixins/numeric.rb
@@ -4,12 +4,16 @@ class Numeric
   # TODO: Use 1024
   def to_bytes
     args = case self.abs.to_i
-    when 0..1_000_000
+    when 0..1_000
+      [(self).to_s, 'B']
+    when 1_000..1_000_000
       [(self / 1000).to_s, 'KB']
     when 1_000_000..1_000_000_000
       [(self / (1000**2)).to_s, 'MB']
     when 1_000_000_000..1_000_000_000_000
       [(self / (1000**3)).to_s, 'GB']
+    when 1_000_000_000_000..1_000_000_000_000_000
+      [(self / (1000**4)).to_s, 'TB']
     else
       [self, 'B']
     end
