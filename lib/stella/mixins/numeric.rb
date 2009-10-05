1a02c6588e5f72a77ffbf716ece13396dd006f41
delano <delano@solutious.com>
2009-10-05 09:39:24 -0400

    Numeric#to_bytes

---
+++ lib/stella/mixins/numeric.rb
@@ -0,0 +1,19 @@
+
+
+class Numeric
+  # TODO: Use 1024
+  def to_bytes
+    args = case self.abs.to_i
+    when 0..1_000_000
+      [(self / 1000).to_s, 'KB']
+    when 1_000_000..1_000_000_000
+      [(self / (1000**2)).to_s, 'MB']
+    when 1_000_000_000..1_000_000_000_000
+      [(self / (1000**3)).to_s, 'GB']
+    else
+      [self, 'B']
+    end
+    '%3.2f%s' % args
+  end
+end
+
