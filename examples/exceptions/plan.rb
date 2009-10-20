3ac863313e86c93e81ee1d8541c461ab3ae1b4a2
delano <delano@solutious.com>
2009-10-20 13:16:47 -0400

    Fixed Array usage in exception example

--- examples/exceptions/plan.rb
+++ examples/exceptions/plan.rb
@@ -9,7 +9,7 @@ usecase "Exception Handling" do
   
   get "/search", "Search Results" do
     param :what  => 'No Such Listing'
-    param :where => random('Toronto', 'Montreal', 'Vancouver')
+    param :where => random(['Toronto', 'Montreal', 'Vancouver'])
     response 404 do
       raise NoSearchResults
     end
