0cf8fd360578f7d39561a9163b106e4f8e681edf
delano <delano@solutious.com>
2009-11-16 17:50:21 -0500

    No need for that fix

--- lib/stella/engine/loadbase.rb
+++ lib/stella/engine/loadbase.rb
@@ -144,17 +144,14 @@ module Stella::Engine
     
     def prepare_dumper(plan, opts)
       Stella::Hand.new(15.seconds, 2.seconds) do
-        unless opts[:"disable-stats"]
-          Benelux.update_global_timeline
-          #reqlog.info [Time.now, Benelux.timeline.size].inspect
-          @reqlog.info Benelux.timeline.messages.filter(:kind => :request)
-          @failog.info Benelux.timeline.messages.filter(:kind => :exception)
-          @failog.info Benelux.timeline.messages.filter(:kind => :timeout)
-          @authlog.info Benelux.timeline.messages.filter(:kind => :authentication)
-          @reqlog.clear and @failog.clear and @authlog.clear
-          #generate_runtime_report(plan)
-        end
-        Benelux.timeline.clear
+        Benelux.update_global_timeline 
+        #reqlog.info [Time.now, Benelux.timeline.size].inspect
+        @reqlog.info Benelux.timeline.messages.filter(:kind => :request)
+        @failog.info Benelux.timeline.messages.filter(:kind => :exception)
+        @failog.info Benelux.timeline.messages.filter(:kind => :timeout)
+        @authlog.info Benelux.timeline.messages.filter(:kind => :authentication)
+        @reqlog.clear and @failog.clear and @authlog.clear
+        Benelux.timeline.clear if opts[:"disable-stats"]
       end
 
     end
