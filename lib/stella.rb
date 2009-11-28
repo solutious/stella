4989d22f686915da6a08fabe979ec346bf7f99fd
delano <delano@solutious.com>
2009-11-28 11:41:17 -0500

    Release 0.7.6.002

--- lib/stella.rb
+++ lib/stella.rb
@@ -23,7 +23,7 @@ module Stella
       MAJOR = 0.freeze
       MINOR = 7.freeze
       TINY  = 6.freeze
-      PATCH = '001'.freeze 
+      PATCH = '002'.freeze 
     end
     def self.to_s; [MAJOR, MINOR, TINY].join('.'); end
     def self.to_f; self.to_s.to_f; end
