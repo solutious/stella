92eedf51adebc1883b60b2ae39741bd2c5a11e5f
delano <delano@solutious.com>
2009-12-13 14:50:27 -0500

    Back on the 0.8 train

--- lib/stella.rb
+++ lib/stella.rb
@@ -21,9 +21,9 @@ module Stella
   module VERSION
     unless defined?(MAJOR)
       MAJOR = 0.freeze
-      MINOR = 7.freeze
-      TINY  = 7.freeze
-      PATCH = '002'.freeze 
+      MINOR = 8.freeze
+      TINY  = 0.freeze
+      PATCH = '900'.freeze 
     end
     def self.to_s; [MAJOR, MINOR, TINY].join('.'); end
     def self.to_f; self.to_s.to_f; end
